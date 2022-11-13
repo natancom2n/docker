ddd0b484ffd3ff59cf0562bd0633dd900f31eda2
NATANAEL ALVES PINTO <natan@NATANAELs-MacBook-Pro.local>
2022-11-13 08:46:25 -0300

    dockerCompose test

---
+++ newNode/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3333
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`Excecute on port: ${port}`)
+})
